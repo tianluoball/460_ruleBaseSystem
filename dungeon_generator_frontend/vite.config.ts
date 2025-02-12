03b17fe2fce4ee35a1787798d5869c2965b8b350
tianluoball <tianluoball@gmail.com>
2025-02-12 12:38:23 -0800

    Update vite.config.ts

--- dungeon_generator_frontend/vite.config.ts
+++ dungeon_generator_frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/460_ruleBaseSystem/dungeon_generator_frontend/', // 这里填写你的 GitHub 仓库名
+  base: '/460_ruleBaseSystem/',
 })
